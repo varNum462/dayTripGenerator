6d36daf9dbb6bdb5de014f01671a5e1beefb59d0
varNum462 <gary@varnumweb.com>
2022-02-18 22:17:55 -0500

    Created function to make random choices from arrays, called it or each array

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ let destinationChoices = ["Washington, D.C.","Philadelphia","Annapolis","Ocean C
 let restaurantChoices = ["McDonald's","Applebee's","Cheesecake Factory","Outback Steakhouse","Red Lobster"];
 let transportationChoices = ["Car","Bus","Train","Plane"]
 let entertainmentChoices = ["Museum","Historical Landmark","Live Band","Pub Crawl","Sporting Event","Wine Tasting","Dog Show"]
+
+function randomChoice(arr,optionType){
+    let chooseOption = Math.floor(Math.random() * arr.length);
+    let optionIndex = chooseOption;
+    console.log(`${optionType}: ${arr[chooseOption]}`);
+}
+
+randomChoice(destinationChoices,"Destination");
+randomChoice(restaurantChoices,"Restaurant");
+randomChoice(transportationChoices,"Transportation");
+randomChoice(entertainmentChoices,"Entertainment");
+
