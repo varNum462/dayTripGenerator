f1d5736759ae4d7e407f08ce910586e449ef7ebf
varNum462 <gary@varnumweb.com>
2022-02-18 21:51:35 -0500

    Create arrays for destination, restaurant, transportation, and entertainment options

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 "use strict";
+
+let destinationChoices = ["Washington, D.C.","Philadelphia","Annapolis","Ocean City","Richmond","New York City"];
+let restaurantChoices = ["McDonald's","Applebee's","Cheesecake Factory","Outback Steakhouse","Red Lobster"];
+let transportationChoices = ["Car","Bus","Train","Plane"]
+let entertainmentChoices = ["Museum","Historical Landmark","Live Band","Pub Crawl","Sporting Event","Wine Tasting","Dog Show"]
