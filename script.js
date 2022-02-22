1360a45d4f420911ca5ebf960ff5905d7363cb8b
varNum462 <gary@varnumweb.com>
2022-02-21 20:04:46 -0500

    Cleaned up initial choices and console log showing trip

--- script.js
+++ script.js
@@ -4,18 +4,21 @@ let destinationChoices = ["Washington, D.C.","Philadelphia","Annapolis","Ocean C
 let restaurantChoices = ["McDonald's","Applebee's","Cheesecake Factory","Outback Steakhouse","Red Lobster"];
 let transportationChoices = ["Car","Bus","Train","Plane"]
 let entertainmentChoices = ["Museum","Historical Landmark","Live Band","Pub Crawl","Sporting Event","Wine Tasting","Dog Show"]
+let tripChoices = [];
 
 function randomChoice(arr,optionType){
     let chooseOption = Math.floor(Math.random() * arr.length);
     let optionIndex = chooseOption;
-    console.log(`${optionType}: ${arr[chooseOption]}`);
-    let optionChoice = `${optionType}: ${arr[chooseOption]}`;
+    let optionChoice = `${optionType}: ${arr[chooseOption]}`;    
+    tripChoices.push = (optionChoice);
     return optionChoice;
 }
 
-randomChoice(destinationChoices,"Destination");
-randomChoice(restaurantChoices,"Restaurant");
-randomChoice(transportationChoices,"Transportation");
-randomChoice(entertainmentChoices,"Entertainment");
+let destination = randomChoice(destinationChoices,"Destination");  
+let restaurant = randomChoice(restaurantChoices,"Restaurant");
+let transportation = randomChoice(transportationChoices,"Transportation");
+let entertainment = randomChoice(entertainmentChoices,"Entertainment");
+
+console.log(`Your random trip:\n ${destination}\n ${restaurant}\n ${transportation}\n ${entertainment}`);
 
 let tripApproval = prompt("Do you like this trip?");
